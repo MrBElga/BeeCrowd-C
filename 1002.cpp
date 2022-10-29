379589b36e62933f1f12a4b07678215b2cb41b08
MrBElga <43758059+MrBElga@users.noreply.github.com>
2022-10-29 16:43:09 -0300

    BeeCrowd

---
+++ 1002.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+    double raio,area;
+    scanf("%lf",&raio);
+    printf("A=%.4lf\n",(3.14159*pow(raio,2)));
+    return 0;
+}
---
+++ 1003.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    int a,b,x;
+    scanf("%d %d", &a, &b);
+    printf("SOMA = %d\n", (a+b));
+    return 0;
+}
